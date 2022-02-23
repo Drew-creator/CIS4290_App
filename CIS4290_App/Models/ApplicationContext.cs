c3438a727733d985f853974ce9320f61cae16c41
Andrew Nguyen <andrew.n99@yahoo.com>
2022-02-22 22:47:43 -0800

    Add project files.

---
+++ CIS4290_App/Models/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityByExamples.Models
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
